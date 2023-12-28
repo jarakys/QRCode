b36ba3c297e0ede712467f49574eeb1d16cdbce1
KyryloChernovUptime <kyrylo.chernov@uptimedevelopment.pl>
2023-12-28 16:55:29 +0200

    Added different icons for Created and Scanned

--- QRCodeApp/Feature/History/Views/Cells/HistoryQrCodeCellView.swift
+++ QRCodeApp/Feature/History/Views/Cells/HistoryQrCodeCellView.swift
@@ -11,9 +11,15 @@ struct HistoryQrCodeCellView: View {
     let model: QRCodeEntityModel
     var body: some View {
         HStack(spacing: 16) {
-            Image(uiImage: UIImage(data: model.image)!)
-                .resizable()
-                .frame(width: 48, height: 48)
+            if model.isCreated {
+                Image(uiImage: UIImage(data: model.image)!)
+                    .resizable()
+                    .frame(width: 48, height: 48)
+            } else {
+                Image(model.qrCodeFormat.image)
+                    .resizable()
+                    .frame(width: 48, height: 48)
+            }
             VStack {
                 Text("Qr Code · \(model.qrCodeFormat.description)")
                     .font(.system(size: 15, weight: .semibold))
