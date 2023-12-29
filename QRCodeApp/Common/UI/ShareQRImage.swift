88744cd801f6f0aea264020e0d187f9ead50052e
KyryloChernovUptime <kyrylo.chernov@uptimedevelopment.pl>
2023-12-29 19:23:47 +0200

    Fixed QRCode Scan

--- QRCodeApp/Common/UI/ShareQRImage.swift
+++ QRCodeApp/Common/UI/ShareQRImage.swift
@@ -4,19 +4,3 @@
 //
 //  Created by Kyrylo Chernov on 25.12.2023.
 //
-
-import SwiftUI
-
-struct ShareQRImage: Transferable {
-    func generateReport() async -> String {
-        print("Generating...")
-        // do some work...
-        return "A generated report"
-    }
-    
-    static var transferRepresentation: some TransferRepresentation {
-        ProxyRepresentation { report in
-            await report.generateReport()
-        }
-    }
-}
--- QRCodeApp/Feature/Scan/Wrapper/ViewController.swift
+++ QRCodeApp/Feature/Scan/Wrapper/ViewController.swift
@@ -29,10 +29,12 @@ struct QRCodeScanner: UIViewControllerRepresentable {
 class ViewController: UIViewController {
     private var captureSession: AVCaptureSession!
     private var previewLayer: QRCodeReaderPreviewLayer!
-    private let metadataOutput = AVCaptureMetadataOutput()
+    private let metadataOutput = AVCaptureVideoDataOutput()
+    
     private var isConfigured = false
     
     private var qrCodeString: String?
+    private var detector: CIDetector? = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: nil)
     
     var callback: ((String) -> Void)?
     
@@ -49,11 +51,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         guard (captureSession?.isRunning ?? false) == false else { return }
-
         if !isConfigured {
             configureQRCodeReader()
         }
-        
         
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             self?.captureSession.startRunning()
@@ -83,8 +83,9 @@ class ViewController: UIViewController {
         
         if captureSession.canAddOutput(metadataOutput) {
             captureSession.addOutput(metadataOutput)
-            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metadataOutput.metadataObjectTypes = [.qr]
+            metadataOutput.alwaysDiscardsLateVideoFrames = true
+            metadataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            metadataOutput.setSampleBufferDelegate(self, queue: .main) //setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         } else {
             captureSession = nil
             return
@@ -110,12 +111,6 @@ class ViewController: UIViewController {
         animationLayer.isHidden = true
         previewLayer.addSublayer(animationLayer)
         self.animationLayer = animationLayer
-        
-        /// Captures only inside of reader square
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.metadataOutput.rectOfInterest = self.previewLayer.rectOfInterest
-        }
     }
     
     private func animateLineToTop() {
@@ -133,6 +128,44 @@ class ViewController: UIViewController {
         
         animationLayer.add(animation, forKey: "moveToTopAnimation")
     }
+    
+    private func isInsideLayer(fullImage: CIImage, feature: CIQRCodeFeature) -> Bool {
+        let midXFullImage = fullImage.extent.midX
+        let midYFullImage = fullImage.extent.midY
+        
+        let maskRect: CGFloat = 0.38785046729 * fullImage.extent.height //Square
+        
+        let rect = CGRect(x: midXFullImage - maskRect / 2,
+                          y: midYFullImage - maskRect / 2,
+                          width: maskRect,
+                          height: maskRect)
+        
+        print("mask rect \(rect)")
+        print("feature rect \(feature.bounds)")
+    
+        print(previewLayer.maskContainer.contains(feature.bounds))
+        print("isRectInsideRect \(isRectInsideRect(innerRect: feature.bounds, outerRect: rect)))")
+        if previewLayer.maskContainer.contains(feature.bounds) {
+            print("niggers")
+        }
+        return isRectInsideRect(innerRect: feature.bounds, outerRect: rect)
+//        if previewLayer.maskContainer.origin.x <= itemCoordinatesInPreviewLayer.origin.x &&
+//            previewLayer.maskContainer.origin.x + 40 >= itemCoordinatesInPreviewLayer.origin.x &&
+//            previewLayer.maskContainer.origin.y >= itemCoordinatesInPreviewLayer.origin.y &&
+//            previewLayer.maskContainer.origin.y + previewLayer.maskContainer.height >= itemCoordinatesInPreviewLayer.origin.y + itemCoordinatesInPreviewLayer.height
+//        {
+//            return true
+//        } else {
+//            return false
+//        }
+    }
+    
+    func isRectInsideRect(innerRect: CGRect, outerRect: CGRect) -> Bool {
+        return innerRect.origin.x >= outerRect.origin.x &&
+               innerRect.origin.y >= outerRect.origin.y &&
+               (innerRect.origin.x + innerRect.size.width) <= (outerRect.origin.x + outerRect.size.width) &&
+               (innerRect.origin.y + innerRect.size.height) <= (outerRect.origin.y + outerRect.size.height)
+    }
 }
 
 // MARK: - CAAnimationDelegate
@@ -145,16 +178,213 @@ extension ViewController: CAAnimationDelegate {
 }
 
 // MARK: - AVCaptureMetadataOutputObjectsDelegate
-extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        captureSession.stopRunning()
-        animateLineToTop()
-        if let metadataObject = metadataObjects.first,
-           metadataObject.type == .qr {
-            let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject
-            guard let qrCodeString = readableObject?.stringValue else { return }
-            self.qrCodeString = qrCodeString
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let imageBuf = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        guard let image = Optional(CIImage(cvImageBuffer: imageBuf)) else { return }
+        guard let features = detector?.features(in: image) as? [CIQRCodeFeature] else { return }
+        features.forEach({ feature in
+            if isInsideLayer(fullImage: image, feature: feature) {
+                qrCodeString = feature.messageString
+                captureSession.stopRunning()
+                animateLineToTop()
+            }
+        })
+    }
+}
+
+
+class ViewController2: UIViewController {
+    let detector = VideoDetector()
+    var previewLayer: QRCodeReaderPreviewLayer?
+    
+    private var isConfigured: Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func convertCoordinates(_ coordinates: CGRect, from sourceRect: CGRect, to destinationRect: CGRect) -> CGRect {
+            let x = (coordinates.origin.x - sourceRect.origin.x) / sourceRect.size.width * destinationRect.size.width
+            let y = (coordinates.origin.y - sourceRect.origin.y) / sourceRect.size.height * destinationRect.size.height
+            let width = coordinates.size.width / sourceRect.size.width * destinationRect.size.width
+            let height = coordinates.size.height / sourceRect.size.height * destinationRect.size.height
+
+            return CGRect(x: x, y: y, width: width, height: height)
+        }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard !isConfigured else { return }
+        isConfigured = true
+        do {
+            try self.detector.startDetecting { [weak self] fullImage, features in
+                guard let self else { return }
+                guard let previewLayer = self.previewLayer else { return }
+                guard let feature = features.first else { return }
+                
+                let itemCoordinatesInPreviewLayer = self.convertCoordinates(feature.bounds, from: fullImage.extent, to: previewLayer.bounds)
+                print(itemCoordinatesInPreviewLayer)
+            
+                print(previewLayer.maskContainer.contains(itemCoordinatesInPreviewLayer))
+                if previewLayer.maskContainer.origin.x <= itemCoordinatesInPreviewLayer.origin.x &&
+                    previewLayer.maskContainer.origin.x + previewLayer.maskContainer.width >= itemCoordinatesInPreviewLayer.origin.x + itemCoordinatesInPreviewLayer.width &&
+                    previewLayer.maskContainer.origin.y <= itemCoordinatesInPreviewLayer.origin.y &&
+                    previewLayer.maskContainer.origin.y + previewLayer.maskContainer.height >= itemCoordinatesInPreviewLayer.origin.y + itemCoordinatesInPreviewLayer.height
+                {
+                    print("Success")
+                }
+                print("[test] maskContainer \(previewLayer.maskContainer)")
+                print("[test] itemCoordinatesInPreviewLayer \(itemCoordinatesInPreviewLayer)")
+                self.updateForQRCodes(features)
+            }
+        }
+        catch {
+            Swift.print("Could not start video capture (error: \(error))")
+            Swift.print("Note that the simulator does not have a video capture device, so you need to run this on a 'real' device")
+            fatalError()
+        }
+
+        let pl = try! self.detector.makePreviewLayer()
+
+//        self.view.layer.addSublayer(pl)
+        let previewLayer = QRCodeReaderPreviewLayer(session: pl.session!,
+                                                    marginSize: 128,
+                                                    drawer: { previewLayer in
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.contents = UIImage(resource: .scanFrameIcon).cgImage
+            shapeLayer.frame = previewLayer.maskContainer
+            return shapeLayer
+        })
+        previewLayer.frame = self.view.layer.bounds
+        previewLayer.backgroundColor = UIColor.clear.cgColor
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        self.previewLayer = previewLayer
+    }
+
+    func updateForQRCodes(_ features: [CIQRCodeFeature]) {
+        Swift.print("\(features.count) QR code(s) detected [")
+        features.forEach { feature in
+//            Swift.print("- \(feature.messageString ?? ""), bounds=\(feature.bounds)")
+        }
+        Swift.print("]")
+    }
+}
+
+
+class VideoDetector: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    /// Errors generated by the video detector
+    public enum CaptureError: Error {
+        case noSession
+        case noDefaultVideoDevice
+        case cannotCreateVideoInput
+        case cannotAddVideoOutput
+    }
+
+    /// The format for the callback block when a QR code is detected in the input stream
+    public typealias DetectionBlock = (CIImage, [CIQRCodeFeature]) -> Void
+
+    /// Start detecting qr codes in a video stream
+    /// - Parameters:
+    ///   - captureQueue: The queue to run the capture session. Defaults to `.global`
+    ///   - queue: The queue to process results on. Defaults to `.global`
+    ///   - inputDevice: the capture input device, or nil to use the system's default video input device
+    ///   - detectionBlock: Called when qr code(s) are detected in the input video stream
+    public func startDetecting(
+        captureQueue: DispatchQueue = .global(),
+        queue: DispatchQueue = .global(),
+        inputDevice: AVCaptureDeviceInput? = nil,
+        _ detectionBlock: @escaping DetectionBlock
+    ) throws {
+        self.detectionBlock = detectionBlock
+
+        var device = inputDevice
+        if device == nil {
+            guard let defaultVideo = AVCaptureDevice.default(for: AVMediaType.video) else {
+                throw CaptureError.noDefaultVideoDevice
+            }
+            device = try AVCaptureDeviceInput(device: defaultVideo)
+        }
+
+        guard let device = device else { throw CaptureError.cannotCreateVideoInput }
+
+        // Create a capture session
+        let session = AVCaptureSession()
+        session.addInput(device)
+
+        // Create a video output
+        let output = AVCaptureVideoDataOutput()
+        guard session.canAddOutput(output) else {
+            throw CaptureError.cannotAddVideoOutput
+        }
+
+        output.alwaysDiscardsLateVideoFrames = true
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+        output.setSampleBufferDelegate(self, queue: queue)
+        session.addOutput(output)
+
+        // Create the QRCode detector
+        self.detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: nil)
+
+        self.captureSession = session
+
+        captureQueue.async {
+            session.startRunning()
+        }
+    }
+
+    /// Returns a `AVCaptureVideoPreviewLayer` which reflects the content of the current capture session
+    public func makePreviewLayer() throws -> AVCaptureVideoPreviewLayer {
+        guard let session = self.captureSession else {
+            throw CaptureError.noSession
+        }
+        let preview = AVCaptureVideoPreviewLayer(session: session)
+        preview.videoGravity = .resizeAspect
+        return preview
+    }
+
+    /// Stop capturing input video
+    public func stopDetection() {
+        if let captureSession = self.captureSession {
+            captureSession.stopRunning()
+        }
+        captureSession = nil
+        self.detectionBlock = nil
+        self.detector = nil
+    }
+
+    deinit {
+        self.stopDetection()
+    }
+
+    // Private
+
+    private var captureSession: AVCaptureSession?
+    private var detector: CIDetector?
+    private var detectionBlock: DetectionBlock?
+
+    private let videoDataOutputQueue = {
+        DispatchQueue(
+            label: "QRCode.VideoDetector",
+            qos: .userInitiated,
+            attributes: [],
+            autoreleaseFrequency: .inherit)
+    }()
+}
+
+extension VideoDetector {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if
+            let block = self.detectionBlock,
+            let imageBuf = CMSampleBufferGetImageBuffer(sampleBuffer),
+            let image = Optional(CIImage(cvImageBuffer: imageBuf)),
+            let features = detector?.features(in: image) as? [CIQRCodeFeature],
+            features.count > 0
+        {
+            block(image, features)
         }
     }
 }
