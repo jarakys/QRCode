59b4e592e7d4ba444c3b0c04d014f21a56ae4165
KyryloChernovUptime <kyrylo.chernov@uptimedevelopment.pl>
2024-01-09 12:03:38 +0200

    Remove unused code

--- QRCodeApp/QRCodeApp.swift
+++ QRCodeApp/QRCodeApp.swift
@@ -19,18 +19,6 @@ struct QRCodeApp: App {
     var showIsPremium: Bool {
         false && closePremium
     }
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Item.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
 
     var body: some Scene {
         WindowGroup {
